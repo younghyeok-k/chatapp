5582b6111d89971dc942a0bdc4ca368f6ffa56e5
younghyeok-k <q6359951@naver.com>
2022-01-07 21:12:19 +0900

    2022/01/07 21:10

--- app/src/main/java/com/example/chatapp/MainActivity.java
+++ app/src/main/java/com/example/chatapp/MainActivity.java
@@ -21,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
              //   Toast.makeText(MainActivity.this,"Login",Toast.LENGTH_LONG).show();
                 Intent in=new Intent(MainActivity.this,ChatActivity.class);
                 startActivity(in);
+                //
             }
         });
     }
