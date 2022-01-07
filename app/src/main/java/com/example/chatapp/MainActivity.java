50954940c5a62ab08224b328997bba6b870299c6
younghyeok-k <q6359951@naver.com>
2022-01-07 21:10:37 +0900

    2022/01/07 21:10

--- app/src/main/java/com/example/chatapp/MainActivity.java
+++ app/src/main/java/com/example/chatapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.chatapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -17,7 +18,9 @@ public class MainActivity extends AppCompatActivity {
         btnlogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this,"Login",Toast.LENGTH_LONG).show();
+             //   Toast.makeText(MainActivity.this,"Login",Toast.LENGTH_LONG).show();
+                Intent in=new Intent(MainActivity.this,ChatActivity.class);
+                startActivity(in);
             }
         });
     }
