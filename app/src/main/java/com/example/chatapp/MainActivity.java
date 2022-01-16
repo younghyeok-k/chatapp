aeeafa7ef99d9ee481ce2bf5a058e7243c3d14ab
younghyeok-k <q6359951@naver.com>
2022-01-17 01:43:16 +0900

    2022/01/17 01:43

--- app/src/main/java/com/example/chatapp/MainActivity.java
+++ app/src/main/java/com/example/chatapp/MainActivity.java
@@ -3,7 +3,9 @@ package com.example.chatapp;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -65,6 +67,11 @@ public class MainActivity extends AppCompatActivity {
                                     String stUserEmail = user.getEmail();
                                     String stUserName = user.getDisplayName();
                                     Log.d(TAG, "stUserEmail: " + stUserEmail + ",stUserName" + stUserName);
+
+                                    SharedPreferences sharedPref =getSharedPreferences("shared",Context.MODE_PRIVATE);
+                                    SharedPreferences.Editor editor = sharedPref.edit();
+                                    editor.putString("email",stUserEmail);
+                                    editor.commit();
                                     // updateUI(user);
                                     Intent in = new Intent(MainActivity.this, TapActivity.class);
                                     in.putExtra("email", stUserEmail);
--- app/src/main/java/com/example/chatapp/ui/notifications/ProfileFragment.java
+++ app/src/main/java/com/example/chatapp/ui/notifications/ProfileFragment.java
@@ -1,8 +1,16 @@
 package com.example.chatapp.ui.notifications;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,17 +19,34 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.chatapp.R;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FileDownloadTask;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.File;
+import java.io.IOException;
 //import com.example.chatapp.databinding.FragmentNotificationsBinding;
 
 public class ProfileFragment extends Fragment {
-
-    int REQUEST_IMAGE_CODE=1001;
+    private static final String TAG = "ProfileFragment";
+    int REQUEST_IMAGE_CODE = 1001;
+    int REQUEST_EXTERNAL_STROAGE_PERMISSION = 1002;
     private NotificationsViewModel notificationsViewModel;
+    StorageReference storageRef;
+    String stEamil;
+    ImageView ivUser;
+
+    File localFile = null;
     // private FragmentNotificationsBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -39,21 +64,99 @@ public class ProfileFragment extends Fragment {
 //                textView.setText(s);
 //            }
 //        });
+        storageRef = FirebaseStorage.getInstance().getReference();
 
-        ImageView ivUser=root.findViewById(R.id.ivUser);
+        SharedPreferences sharedPref = getActivity().getSharedPreferences("shared", Context.MODE_PRIVATE);
+        stEamil = sharedPref.getString("email", "");
+        Log.d(TAG, "stemail:" + stEamil);
+
+
+        if (ContextCompat.checkSelfPermission(
+                getActivity(), Manifest.permission.READ_EXTERNAL_STORAGE) !=
+                PackageManager.PERMISSION_GRANTED) {
+            // You can use the API that requires the permission.
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(getActivity(),
+                    Manifest.permission.READ_EXTERNAL_STORAGE)) {
+
+            } else {
+                ActivityCompat.requestPermissions(getActivity(),
+                        new String[]{Manifest.permission.READ_CONTACTS},
+                        REQUEST_EXTERNAL_STROAGE_PERMISSION);
+            }
+        } else {
+
+        }
+
+
+        ivUser = root.findViewById(R.id.ivUser);
         ivUser.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent in=new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-               startActivityForResult(in,1001);
+                Intent in = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(in, 1001);
             }
         });
+
+
+        try {
+            StorageReference riversRef = storageRef.child("users").child(stEamil).child("profile.jpg");
+            localFile = File.createTempFile("images", "jpg");
+            riversRef.getFile(localFile)
+                    .addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
+
+                            Bitmap bitmap = BitmapFactory.decodeFile(localFile.getAbsolutePath());
+                            ivUser.setImageBitmap(bitmap);
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+
+                }
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
         return root;
     }
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_CODE) {
+            Uri image = data.getData();
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(), image);
+                ivUser.setImageBitmap(bitmap);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            StorageReference riversRef = storageRef.child("users").child(stEamil).child("profile.jpg");
+
+            riversRef.putFile(image)
+                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            Log.d(TAG, taskSnapshot.toString());
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+
+                        }
+                    });
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 }
 //    @Override
